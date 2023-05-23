59702dcc2e34c84830e380e5858bae927d199dfe
miusarname <oscarm.alvarezg@gmail.com>
2023-05-23 15:52:39 -0500

    Se sube las actualizaciones

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-const fecha = new Date();
+//const fecha = new Date();
+const listBuy=document.querySelector('.doBuys')
+const addCart= document.querySelectorAll('.addCart')
 
-console.log(fecha);
+
+
+const newShopping = (object)=>{
+    listBuy.innerHTML +=  `  <li class="nav-link"><a href="#"><i class='bx bxs-t-shirt icon' ></i><span class="text nav-text">${object}</span></a></li> `
+};
+//console.log(fecha);
 
 const body = document.querySelector('body'),
     sidebar = body.querySelector('nav'),
@@ -14,18 +21,13 @@ toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click", () => {
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-
-    }
+console.log("cart: " + addCart);
+addCart.forEach(el=>{
+    el.addEventListener('click', () => {
+        let content= el.parentElement.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.textContent;
+        let contents=el.parentNode
+        console.log("content: " + content);
+        newShopping(content);
+        
+    });
 });
-
